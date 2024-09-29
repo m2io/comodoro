2047103053b41ff42d365c6bbdea883ae92b12c0
m2io <grmnn@pm.me>
2024-09-29 21:07:00 +0200

    chore(refactor: only return needed user agent info

--- app/types/site.ts
+++ app/types/site.ts
@@ -2,9 +2,4 @@ export interface UserAgentInfo {
 	isMac: boolean
 	isWindows: boolean
 	isLinux: boolean
-	isChrome: boolean
-	isFirefox: boolean
-	isSafari: boolean
-	isEdge: boolean
-	isOpera: boolean
 }
