46657ad2ccbb91993fd03ef250317f21daaf5c5b
m2io <grmnn@pm.me>
2024-09-29 21:07:21 +0200

    fix(style): apply z-index to modal

--- app/components/base/BaseModal.vue
+++ app/components/base/BaseModal.vue
@@ -23,7 +23,7 @@ function onBackdropClick() {
 		>
 			<div
 				v-if="modelValue"
-				class="fixed inset-0 bg-zinc-900/30 backdrop-blur-md"
+				class="fixed inset-0 bg-zinc-900/30 backdrop-blur-md z-50"
 				@click="onBackdropClick"
 			></div>
 		</Transition>
@@ -39,7 +39,7 @@ function onBackdropClick() {
 		>
 			<div
 				v-if="modelValue"
-				class="absolute top-24 w-full max-w-4xl inset-x-1/2 -translate-x-1/2 z-[1020] flex flex-col overflow-hidden rounded-xl border border-zinc-800 bg-zinc-900 will-change-transform px-8 py-6 shadow-modal"
+				class="absolute top-24 w-full max-w-4xl inset-x-1/2 -translate-x-1/2 z-[1020] flex flex-col overflow-hidden rounded-xl border border-zinc-800 bg-zinc-900 will-change-transform px-8 py-6 shadow-modal z-100"
 				:class="{
 					'inset-y-24': fullHeight,
 				}"
